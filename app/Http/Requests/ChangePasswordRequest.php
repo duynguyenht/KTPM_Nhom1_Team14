502c7d7467afc6eb9bc463eeea25bd65a3c6b640
Nguyen Hoang Duy <116094811+duynguyenht@users.noreply.github.com>
2024-05-20 19:52:57 +0700

    Update ChangePasswordRequest.php

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+
 
 class ChangePasswordRequest extends FormRequest
 {
