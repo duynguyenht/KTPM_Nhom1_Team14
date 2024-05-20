a323e8115282f04b37301f03164cceb0b79493e7
Nguyen Hoang Duy <116094811+duynguyenht@users.noreply.github.com>
2024-05-20 20:03:07 +0700

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,5 @@
 <?php
-
+<?
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Admin\DashboardController;
