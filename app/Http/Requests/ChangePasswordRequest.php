37234f27f0a80109b971062ee4b930c6c5d21c4e
Nguyen Hoang Duy <116094811+duynguyenht@users.noreply.github.com>
2024-05-21 07:49:31 +0700

    Update ChangePasswordRequest.php

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -55,14 +55,7 @@ class ChangePasswordRequest extends FormRequest
      * @param Validator $validator
      * @return void
      */
-    public function withValidator(Validator $validator)
-    {
-        $validator->after(function ($validator) {
-            if ($this->matchCurrentPassword($this->current_password)) {
-                $validator->errors()->add('current_password', 'Mật khẩu hiện tại không đúng');
-            }
-        });
-    }
+   DIA
 
     /**
      * Check current password
