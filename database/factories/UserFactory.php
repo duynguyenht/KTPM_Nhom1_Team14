257551e57425f205ce41641faf1589c7ee36b079
Nguyendominhquan <162933279+Nguyendominhquan@users.noreply.github.com>
2024-05-23 14:52:02 +0700

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,7 +33,7 @@ class UserFactory extends Factory
      *
      * @return static
      */
-    public function unverified()
+     public function unverified()
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
